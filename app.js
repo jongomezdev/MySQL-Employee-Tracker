9f71482098439cee50ee7997679cbce6aef53990
Jonathan Gomez <jongomezdev@gmail.com>
2021-02-03 17:10:44 -0500

    Complete switch statement

--- app.js
+++ app.js
@@ -26,20 +26,49 @@ const connection = mysql.createConnection({
 });
 
 async function searchDB() {
-  inquirer.prompt({
-    name: "choice",
-    type: "list",
-    message: "Welcome, please select an action:",
-    choices: [
-      "View all departments",
-      "View all roles",
-      "View all employees",
-      "Add a department",
-      "Add a role",
-      "Add an employee",
-      "Update roles",
-    ],
-  });
+  inquirer
+    .prompt({
+      name: "choice",
+      type: "list",
+      message: "Welcome, please select an action:",
+      choices: [
+        "View all departments",
+        "View all roles",
+        "View all employees",
+        "Add a department",
+        "Add a role",
+        "Add an employee",
+        "Update employee roles",
+        "Exit",
+      ],
+    })
+    .then(({ choice }) => {
+      switch (choice) {
+        case "View all departments":
+          viewDep();
+          break;
+        case "View all roles":
+          viewRoles();
+          break;
+        case "View all employees":
+          viewEmp();
+          break;
+        case "Add a department":
+          addDep();
+          break;
+        case "Add a role":
+          addRole();
+          break;
+        case "Add an employee":
+          addEmp();
+          break;
+        case "Update employee roles":
+          update();
+          break;
+        default:
+          connection.end();
+      }
+    });
 }
 
 // Add departments, roles, employees
