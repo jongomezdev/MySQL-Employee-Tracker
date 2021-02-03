f190c38aea40a5e29604a4f1fb8f06de66e758b2
Jonathan Gomez <jongomezdev@gmail.com>
2021-02-03 09:27:23 -0500

    Run test for figlet and color packages

--- app.js
+++ app.js
@@ -3,3 +3,11 @@ const inquirer = require("inquirer");
 const cTable = require("console.table");
 const figlet = require("figlet");
 const colors = require("colors");
+
+figlet("Employee Tracker", function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+  }
+  console.log(data.rainbow);
+});
