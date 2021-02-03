10b725f2f660e17ea1e5675703dda04d50404319
Jonathan Gomez <jongomezdev@gmail.com>
2021-02-03 13:22:47 -0500

    Complete insert into role

---
+++ seed.sql
@@ -0,0 +1,52 @@
+USE employee_tracker;
+
+INSERT INTO department (name)
+VALUES ('Medical Staff'),
+('Administrative'),
+('Marketing');
+
+INSERT INTO role (title, salary, department_id)
+VALUES ('Doctor', 300000.00, 1),
+('Medical Assistant', 129000.00, 1),
+('Receptionist', 30000.00, 2),
+('Bookkeeper', 30000.00, 2),
+('Digital Marketer', 80000.00, 3),
+('Frontend Developer', 60000.00, 3);
+
+
+-- office manager
+
+-- receptionist 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- private practice
+-- doctor
+-- nurse
+-- pa
+-- rn
+
+-- medical assitant
+-- medical scribe
+
+-- receptionist
+-- billers
+-- bookeepers
+
+-- marketing
