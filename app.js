5136d1ad67a3d38efe681094ca48ad5915eee7a1
Jonathan Gomez <jongomezdev@gmail.com>
2021-02-03 09:22:43 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,5 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const cTable = require("console.table");
+const figlet = require("figlet");
+const colors = require("colors");
