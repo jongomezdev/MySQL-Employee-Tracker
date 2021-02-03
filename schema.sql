2dc0ef75aefd620364d48fd6915e506b0431bbc9
Jonathan Gomez <jongomezdev@gmail.com>
2021-02-03 10:30:47 -0500

    create employee table

--- schema.sql
+++ schema.sql
@@ -3,16 +3,25 @@ CREATE DATABASE employee_tracker;
 
 USE employee_tracker;
 
-CREATE TABLE department(
+CREATE TABLE department (
   id INT AUTO_INCREMENT NOT NULL,
   name VARCHAR(30),
   PRIMARY KEY (id)
 )
 
-CREATE TABLE role(
+CREATE TABLE role (
   id INT AUTO_INCREMENT NOT NULL,
   title VARCHAR(30),
-  salary DECIMAL
-  department_id INT
+  salary DECIMAL,
+  department_id INT NOT NULL,
+  PRIMARY KEY (id)
 )
 
+CREATE TABLE employee (
+  id INT AUTO_INCREMENT NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT,
+  PRIMARY KEY (id)
+)
