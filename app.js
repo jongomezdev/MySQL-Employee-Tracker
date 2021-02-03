28eae2905b9118d058d9a610247ba5687eed98ea
Jonathan Gomez <jongomezdev@gmail.com>
2021-02-03 17:00:54 -0500

    Create list of choices in the start menu

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cTable = require("console.table");
 const figlet = require("figlet");
 const colors = require("colors");
 
+// Opening Graphic
 figlet("Employee Tracker", function (err, data) {
   if (err) {
     console.log("Something went wrong...");
@@ -12,6 +13,7 @@ figlet("Employee Tracker", function (err, data) {
   console.log(data.rainbow);
 });
 
+// Establish connection with database
 const connection = mysql.createConnection({
   host: "localhost",
 
@@ -20,5 +22,28 @@ const connection = mysql.createConnection({
   user: "root",
 
   password: "~HG)9Pdw9Y4ZgnPW",
-  database: "top_songsDB",
+  database: "employee_tracker",
 });
+
+async function searchDB() {
+  inquirer.prompt({
+    name: "choice",
+    type: "list",
+    message: "Welcome, please select an action:",
+    choices: [
+      "View all departments",
+      "View all roles",
+      "View all employees",
+      "Add a department",
+      "Add a role",
+      "Add an employee",
+      "Update roles",
+    ],
+  });
+}
+
+// Add departments, roles, employees
+
+// View departments, roles, employees
+
+// Update employee roles
