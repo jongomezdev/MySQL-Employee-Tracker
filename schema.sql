ae71c53355700c2aff941ba36ec3f88de648bd85
Jonathan Gomez <jongomezdev@gmail.com>
2021-02-08 08:29:17 -0500

    fix schema and seed to work properly

--- schema.sql
+++ schema.sql
@@ -3,25 +3,48 @@ CREATE DATABASE employee_tracker;
 
 USE employee_tracker;
 
-CREATE TABLE department (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30)
+CREATE TABLE department(
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30),
+  PRIMARY KEY (id)
 );
 
-CREATE TABLE role (
-  id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE role(
+  id INT AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL,
-  salary DECIMAL NOT NULL,
-  department_id INT NOT NULL,
-  FOREIGN KEY(department_id) REFERENCES department(id)
+  salary DECIMAL,
+  department_id INT,
+  PRIMARY KEY (id)
 );
 
-CREATE TABLE employee (
+CREATE TABLE employee(
   id INT AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INT NOT NULL,
+  role_id INT,
   manager_id INT,
-  FOREIGN KEY (role_id) REFERENCES role(id),
   FOREIGN KEY(manager_id) REFERENCES employee(id)
 );
+
+INSERT INTO department(name)
+VALUES ('Medical Staff'),
+('Administrative'),
+('Marketing');
+
+INSERT INTO role(title, salary, department_id)
+VALUES ('Doctor', 300000.00, 1),
+('Medical Assistant', 129000.00, 1),
+('Receptionist', 30000.00, 2),
+('Bookkeeper', 30000.00, 2),
+('Digital Marketer', 80000.00, 3),
+('Frontend Developer', 60000.00, 3);
+
+
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+VALUES ('Jayden', 'Seward', 2, 1),
+('Nikolas', 'Abbott', 1, NULL),
+('Lacy', 'Morris', 4, 3),
+('Braiden', 'Emmet', 3, NULL),
+('Chad', 'Forest', 6, 5),
+('Mackenzie', 'Pit', 5, NULL);
+
--- seed.sql
+++ seed.sql
@@ -1,11 +1,11 @@
 USE employee_tracker;
 
-INSERT INTO department (name)
+INSERT INTO department(name)
 VALUES ('Medical Staff'),
 ('Administrative'),
 ('Marketing');
 
-INSERT INTO role (title, salary, department_id)
+INSERT INTO role(title, salary, department_id)
 VALUES ('Doctor', 300000.00, 1),
 ('Medical Assistant', 129000.00, 1),
 ('Receptionist', 30000.00, 2),
@@ -14,11 +14,10 @@ VALUES ('Doctor', 300000.00, 1),
 ('Frontend Developer', 60000.00, 3);
 
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
 VALUES ('Jayden', 'Seward', 2, 1),
 ('Nikolas', 'Abbott', 1, NULL),
 ('Lacy', 'Morris', 4, 3),
 ('Braiden', 'Emmet', 3, NULL),
 ('Chad', 'Forest', 6, 5),
 ('Mackenzie', 'Pit', 5, NULL);
-
